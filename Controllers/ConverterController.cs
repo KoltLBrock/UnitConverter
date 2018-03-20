f3de79fcd28fc9f11ffce135051775c6d16458cb
KoltLBrock <35542746+KoltLBrock@users.noreply.github.com>
2018-03-20 16:33:31 -0500

    half done, needs html part

---
+++ Controllers/ConverterController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UnitConverter.Models;
+
+namespace UnitConverter.Controllers
+{
+    public class ConverterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Wizard(){
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Convert(UnitOperation unitOp){
+            if(ModelState.IsValid){
+                switch(unitOp.converstionType){
+                    case "Speed":
+                        switch(unitOp.inputType){
+                            case "MPH":
+                                //1.60934 is conversion rate from mph to km per hour (source: google convert)
+                                unitOp.result = unitOp.input * (float)1.60934;
+                                break;
+                            case "KMPH":
+                                //0.6213671 is conversion rate from kmph to mph (source: google convert)
+                                unitOp.result = unitOp.input * (float)0.621371;
+                                break;
+                        }
+                        break;
+                    case "Temperature":
+                        switch(unitOp.inputType){
+                            case "Farenheit":
+                                unitOp.result = (unitOp.input - (float)32) / (float)1.8;
+                                break;
+                            case "Celcius":
+                                unitOp.result = unitOp.input * (float)1.8 + (float)32;
+                                break;
+                        }
+                        break;
+                }
+            }else{
+                return View("Error");
+            }
+            return View(unitOp);
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/UnitOperation.cs
@@ -0,0 +1,9 @@
+using System;
+namespace UnitConverter.Models{
+    public class UnitOperation{
+        public float input{get; set;}
+        public string converstionType{get; set;}
+        public string inputType{get; set;}
+        public float result{get; set;}
+    }
+}
